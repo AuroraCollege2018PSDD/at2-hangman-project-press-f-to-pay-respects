3e774a1e677f90dcd23a7d1165096ef1a077af80
Dat_Magpie <36179498+CaitlinJohansson@users.noreply.github.com>
2018-05-17 08:59:26 +1000

    Update template.py
    names and proto

--- template.py
+++ template.py
@@ -3,10 +3,10 @@ A longer description of the module with details that may help the user or anybod
 reviewing the code later. make sure you outline in detail what the module does and how it can be used.
 """
 
-__author__ = "Your Name"
+__author__ = "Caitlin, Tahleia"
 __copyright__ = "Copyright 2018, NSW Dept. Ed."
 __license__ = "GPL"
 __version__ = "1.0.1"
 __maintainer__ = "Your Name again unless you are cheating"
 __email__ = "your.addressk@education.nsw.com.au"
-__status__ = "Prototype, Development or Production"
+__status__ = "Prototype"
